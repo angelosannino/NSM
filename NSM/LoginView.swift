4e653256eae0f72170ee2e59b50c656da8f318c8
Angelo Sannino <angelo.sannino@gmail.com>
2023-01-22 17:30:19 +0100

    feat: Add Firebase SPM

---
+++ NSM/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  NSM
+//
+//  Created by Angelo Sannino on 22/01/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- NSM/NSMApp.swift
+++ NSM/NSMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NSMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
