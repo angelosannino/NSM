36c40ee5a2102e3b8790530f4d8036153054f775
Angelo Sannino <angelo.sannino@gmail.com>
2023-01-22 16:09:07 +0100

    Initial Commit

---
+++ NSM/NSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSMApp.swift
+//  NSM
+//
+//  Created by Angelo Sannino on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct NSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
